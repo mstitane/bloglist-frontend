aaa68c4d13627dad4c97b48ff132682e64542c4c
mstitane <mohamed.stitane@opencellsoft.com>
2023-01-19 17:28:08 +0100

    finished exercise 5.1

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import {useState, useEffect} from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    useEffect(() => {
+        blogService.getAll().then(blogs =>
+            setBlogs(blogs)
+        )
+    }, [])
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const user = await loginService.login({
+                username, password,
+            })
+            console.log(user)
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage('Wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
+    const loginForm = () => (
+        <div>
+            <h2>Log in to application</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    username
+                    <input
+                        type="text"
+                        value={username}
+                        name="Username"
+                        onChange={({target}) => setUsername(target.value)}
+                    />
+                </div>
+                <div>
+                    password
+                    <input
+                        type="password"
+                        value={password}
+                        name="Password"
+                        onChange={({target}) => setPassword(target.value)}
+                    />
+                </div>
+                <button type="submit">login</button>
+            </form>
+        </div>
+    )
+
+    const blogsList = () => (
+        <div>
+            <p>{user.name} logged-in</p>
+            <p>
+                {blogs.map(obj => <Blog key={obj.id} blog={obj}/>)}
+            </p>
+        </div>
+    )
+
+    return (
+        <div>
+            <h2>blogs</h2>
+            {user === null ? loginForm() : blogsList()}
+
+        </div>
+    )
+}
+
+export default App
