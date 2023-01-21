fdf28eceb85bfe061d06fbf7cc3de059f78c9799
mstitane <mohamed.stitane@opencellsoft.com>
2023-01-21 23:24:21 +0100

    finished exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,28 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from "react";
+
+const Blog = ({blog}) => {
+    const [visible, setVisible] = useState(false)
+
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+    const showWhenVisible = { display: visible ? '' : 'none' }
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+    return (
+        <div className="blog">
+            <div style={hideWhenVisible}>
+                {blog.title} <button onClick={toggleVisibility}>view</button>
+            </div>
+            <div style={showWhenVisible}>
+                <div> {blog.title} <button onClick={toggleVisibility}>hide</button> </div>
+                <div> {blog.url}</div>
+                <div> likes {blog.likes} <button onClick="">like</button></div>
+                <div> {blog.author}</div>
+
+            </div>
+        </div>
+    )
+}
 
 export default Blog
